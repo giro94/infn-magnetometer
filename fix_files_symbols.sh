29fd19f19eb16a210351ca882069bd0c846c503d
giro94 <giro94@libero.it>
2024-01-16 12:41:09 +0100

    Added minimal prinout

--- fix_files_symbols.sh
+++ fix_files_symbols.sh
@@ -11,6 +11,7 @@ ext=${2:-csv}
 
 if [ -d "${folder}" ] ; then
 	sed -i -e 's/\xE2\x88\x9E/10000/g' -e 's/NaN/10000/g' -e 's/inf/10000/g' ${folder}/*.${ext}
+	echo "Completed."
 else
 	echo "The argument you provided is not a folder! (${folder})"
 fi
